e547c16694cde8eb2bd7418df8c28dd9d99c23ca
DmitryMarkovnikov <markovikovd@gmail.com>
2015-09-20 15:45:26 +0300

    Finished login view

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,8 @@ from wtforms import Form
 from wtforms import TextField, BooleanField
 from wtforms.validators import Required
 
+
 class LoginForm(Form):
-    open_id = TextField('openid', validators=[Required()])
+    openid = TextField('open_id', validators=[Required()])
     remember_me = BooleanField('remember_me', default=False)
 
--- app/views.py
+++ app/views.py
@@ -1,7 +1,6 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, request
 from forms import LoginForm
-
 
 
 @app.route('/')
@@ -26,7 +25,14 @@ def index():
     return render_template("index.html", title="Home", user=user, posts=posts)
 
 
-@app.route('/login', methods = ['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    return render_template("login.html", title="Sign In", form=form)
+    # You have to initialize the form instance with values from the request:
+    form = LoginForm(request.form)
+    if request.method == "POST" and form.validate():
+        flash('Login submitted with OpenID: "{0}" and'
+              ' with remember_me: "{1}"'.format(form.open_id.data,
+                                                form.remember_me.data))
+        return redirect("/index")
+    return render_template("login.html", title="Sign In", form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
